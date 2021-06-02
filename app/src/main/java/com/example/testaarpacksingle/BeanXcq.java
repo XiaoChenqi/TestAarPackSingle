14b73e9ac605e4a389511a4ed44984fd6913242b
xiaocq <516563076@qq.com>
2021-06-02 15:34:06 +0800

    1

---
+++ app/src/main/java/com/example/testaarpacksingle/BeanXcq.java
@@ -0,0 +1,22 @@
+package com.example.testaarpacksingle;
+
+public class BeanXcq {
+    private     String asd;
+    private    String zxc;
+
+    public String getAsd() {
+        return asd;
+    }
+
+    public void setAsd(String asd) {
+        this.asd = asd;
+    }
+
+    public String getZxc() {
+        return zxc;
+    }
+
+    public void setZxc(String zxc) {
+        this.zxc = zxc;
+    }
+}
---
+++ ib1/src/main/java/com/example/ib1/Constant.java
@@ -0,0 +1,14 @@
+package com.example.ib1;
+
+public  class Constant {
+    private static  final  String xcq="xcq";
+    private static  final String qxc="qxc";
+
+    public static String getXcq() {
+        return xcq;
+    }
+
+    public static String getQxc() {
+        return qxc;
+    }
+}
