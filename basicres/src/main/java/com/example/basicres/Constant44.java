c348ae54a9822189a7333e60f93c2a402cbf61fa
xiaocq <516563076@qq.com>
2021-06-02 18:34:04 +0800

    1

---
+++ basicres/src/main/java/com/example/basicres/Constant44.java
@@ -0,0 +1,14 @@
+package com.example.basicres;
+
+public  class Constant44 {
+    private static  final  String xcq="xcq";
+    private static  final String qxc="qxc";
+
+    public static String getXcq() {
+        return xcq;
+    }
+
+    public static String getQxc() {
+        return qxc;
+    }
+}
